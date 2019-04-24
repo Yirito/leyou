3e26a1ca07b978943bfcf8cb9af52f7875da792e
Tony <tony@gmail.com>
2019-04-24 10:12:52 +0800

    设置工具类

---
+++ ly-common/src/main/java/com/leyou/commom/utils/JsonUtils.java
@@ -0,0 +1,124 @@
+package com.leyou.commom.utils;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.istack.internal.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author: HuYi.Zhang
+ * @create: 2018-04-24 17:20
+ **/
+public class JsonUtils {
+
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+    private static final Logger logger = LoggerFactory.getLogger(JsonUtils.class);
+
+    @Nullable
+    public static String serialize(Object obj) {
+        if (obj == null) {
+            return null;
+        }
+        if (obj.getClass() == String.class) {
+            return (String) obj;
+        }
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            logger.error("json序列化出错：" + obj, e);
+            return null;
+        }
+    }
+
+    @Nullable
+    public static <T> T parse(String json, Class<T> tClass) {
+        try {
+            return mapper.readValue(json, tClass);
+        } catch (IOException e) {
+            logger.error("json解析出错：" + json, e);
+            return null;
+        }
+    }
+
+    @Nullable
+    public static <E> List<E> parseList(String json, Class<E> eClass) {
+        try {
+            return mapper.readValue(json, mapper.getTypeFactory().constructCollectionType(List.class, eClass));
+        } catch (IOException e) {
+            logger.error("json解析出错：" + json, e);
+            return null;
+        }
+    }
+
+    @Nullable
+    public static <K, V> Map<K, V> parseMap(String json, Class<K> kClass, Class<V> vClass) {
+        try {
+            return mapper.readValue(json, mapper.getTypeFactory().constructMapType(Map.class, kClass, vClass));
+        } catch (IOException e) {
+            logger.error("json解析出错：" + json, e);
+            return null;
+        }
+    }
+
+    @Nullable
+    public static <T> T nativeRead(String json, TypeReference<T> type) {
+        try {
+            return mapper.readValue(json, type);
+        } catch (IOException e) {
+            logger.error("json解析出错：" + json, e);
+            return null;
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class User {
+        String name;
+        Integer age;
+    }
+
+    /**
+     * 其实可以使用GSon或者fastJson
+     * @param args
+     */
+    public static void main(String[] args) {
+        User jack = new User("Jack", 21);
+        //序列化
+        //String json = serialize(jack);
+        //System.out.println("json = " + json);
+
+        //反序列化
+        //User user = parse(json, User.class);
+        //System.out.println("user = " + user);
+
+        //集合
+        //String json = "[20,-10,5,15]";
+        //List<Integer> integers = parseList(json, Integer.class);
+        //System.out.println("integers = " + integers);
+
+        //map
+        //String json = "{\"name\":\"Jack\",\"age\":\"21\"}";
+        //Map<String, String> stringStringMap = parseMap(json, String.class, String.class);
+        //System.out.println("stringStringMap = " + stringStringMap);
+
+        //复杂的序列化
+        String json = "[{\"name\":\"Jack\",\"age\":\"21\"},{\"name\":\"Rose\",\"age\":\"18\"},{\"name\":\"XI\",\"age\":\"24\"}]";
+        List<Map<String, String>> maps = nativeRead(json, new TypeReference<List<Map<String, String>>>() {
+        });
+        for (Map<String, String> map : maps) {
+            System.out.println("map = " + map);
+        }
+    }
+}
