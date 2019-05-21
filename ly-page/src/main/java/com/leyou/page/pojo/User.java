d68ba68d860516413cbc28eca78643898406d93e
Tony <tony@gmail.com>
2019-05-21 11:48:26 +0800

    thymeleaf基本语法

---
+++ ly-page/src/main/java/com/leyou/page/pojo/User.java
@@ -0,0 +1,16 @@
+package com.leyou.page.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    String name;
+    int age;
+    User friend;//对象类型属性
+
+}
--- ly-page/src/main/java/com/leyou/page/web/HelloController.java
+++ ly-page/src/main/java/com/leyou/page/web/HelloController.java
@@ -1,15 +1,24 @@
 package com.leyou.page.web;
 
+import com.leyou.page.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String toHello(Model model) {
-        model.addAttribute("msg","hello,thymeleaf!");//这个查看hello.html文件
+        //model.addAttribute("msg","hello,thymeleaf!");//这个查看hello.html文件
+        User user = new User();
+        user.setAge(21);
+        user.setName("Jack Chen");
+        user.setFriend(new User("李小龙", 30, null));
+        User user2 = new User("李小龙", 30, null);
+        model.addAttribute("users", Arrays.asList(user,user2));
         /**
          * 普通字符串被当成识图名称，结合前缀和后缀寻找视图
          * 查看ThymeleafAutoConfiguration源码发现，自动配置，会在java或资源目录下的templates找到hello.html文件
