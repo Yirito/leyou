38cd8e2875c6081824eb3583007c5cc2230435e5
Tony <tony@gmail.com>
2019-05-28 11:55:57 +0800

    发送注册短信

--- ly-user/ly-user-service/src/main/java/com/leyou/user/sercvice/UserService.java
+++ ly-user/ly-user-service/src/main/java/com/leyou/user/sercvice/UserService.java
@@ -2,16 +2,31 @@ package com.leyou.user.sercvice;
 
 import com.leyou.common.enums.ExceptionEnum;
 import com.leyou.common.exception.LyException;
+import com.leyou.common.utils.NumberUtils;
 import com.leyou.user.mapper.UserMapper;
 import com.leyou.user.pojo.User;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    private static final String KEY_PREFIX = "user:verify:phone:";
 
     public Boolean checkData(String data, Integer type) {
         User record = new User();
@@ -40,5 +55,21 @@ public class UserService {
 //        }
 
         return count == 0;
+    }
+
+    public void sendCode(String phone) {
+        //校验手机号什么的，给前端做
+        //生成Redis Key
+        String key = KEY_PREFIX + phone;
+        //生成验证码
+        String code = NumberUtils.generateCode(6);//生成6位数随机验证码
+        Map<String, String> msg = new HashMap<>();
+        msg.put("phone", phone);
+        msg.put("code", code);
+        //发送验证码
+        amqpTemplate.convertAndSend("ly.sms.exchange", "sms.verify.code", msg);
+
+        //保存验证码
+        redisTemplate.opsForValue().set(key,code,5, TimeUnit.MINUTES);
     }
 }
--- ly-user/ly-user-service/src/main/java/com/leyou/user/web/UserController.java
+++ ly-user/ly-user-service/src/main/java/com/leyou/user/web/UserController.java
@@ -2,10 +2,9 @@ package com.leyou.user.web;
 
 import com.leyou.user.sercvice.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -18,10 +17,22 @@ public class UserController {
      *
      * @param data
      * @param type 1用户名,2手机号
-     * @return
+     * @return 返回布尔值
      */
     @GetMapping("/check/{data}/{type}")
     public ResponseEntity<Boolean> checkData(@PathVariable("data") String data, @PathVariable("type") Integer type) {
         return ResponseEntity.ok(userService.checkData(data, type));
+    }
+
+    /**
+     * 发送短信
+     *
+     * @param phone
+     * @return 无返回值
+     */
+    @PostMapping("code")
+    public ResponseEntity<Void> sendCode(@RequestParam("phone") String phone) {
+        userService.sendCode(phone);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
